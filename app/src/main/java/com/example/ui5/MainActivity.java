74da64f4651f29b674d23df9acfea183c7217c1e
zvhir <zahir@simonix.my>
2021-07-09 15:56:21 +0800

    Second commit

--- app/src/main/java/com/example/ui5/MainActivity.java
+++ app/src/main/java/com/example/ui5/MainActivity.java
@@ -3,14 +3,51 @@ package com.example.ui5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    LottieAnimationView greenDotHome;
+    LottieAnimationView greenDotAway;
+
+    ImageView dot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        greenDotHome = findViewById(R.id.greendot_home);
+        greenDotHome.setVisibility(View.INVISIBLE);
+
+        dot = findViewById(R.id.dot);
+        dot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Dialog dialog = new Dialog(MainActivity.this);
+                dialog.setContentView(R.layout.dialog);
+                dialog.show();
+
+                Button closeBtn = (Button) dialog.findViewById(R.id.close);
+                closeBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+
+
 
     }
 }
